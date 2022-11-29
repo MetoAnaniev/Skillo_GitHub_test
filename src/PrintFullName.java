4f64863ef467e3b4a434d0bd85a4deb95f034bfe
MetoAnaniev <metoananiev@gmail.com>
2022-11-29 14:09:50 +0200

    First Commit

---
+++ src/PrintFullName.java
@@ -0,0 +1,12 @@
+public class PrintFullName {
+
+    public static void main(String[] args) {
+
+        String firstName = "Metodi";
+        String fatherName = "Aleksandrov";
+        String lastName = "Ananiev";
+
+        System.out.println(firstName +" "+ fatherName +" "+ lastName);
+
+    }
+}
---
+++ src/javaTrening.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class javaTrening {
+    public static void main(String[] args) {
+        int [] number = {2 , 3, 4, 1, 5};
+        Arrays.sort(number);
+
+        System.out.println(Arrays.toString(number));
+    }
+}
